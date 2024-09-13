37aab32f28c59128627a7656f56789ddefd47e05
Artem Efremov <qazar12321@yandex.ru>
2024-09-13 18:34:39 +0500

    created employers

---
+++ employers/internal/service/service.go
@@ -0,0 +1,37 @@
+package service
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/qaZar1/HHforURFU/employers/autogen"
+	"github.com/qaZar1/HHforURFU/employers/internal/database"
+)
+
+type Service struct {
+	db database.Database
+}
+
+func NewService(db *sqlx.DB) *Service {
+	return &Service{
+		db: *database.NewDatabase(db),
+	}
+}
+
+func (srv *Service) GetAllUsers() ([]autogen.Info, error) {
+	return srv.db.GetAllUsers()
+}
+
+func (srv *Service) GetUserByChatID(chatId int64) (*autogen.Info, error) {
+	return srv.db.GetUserByChatID(chatId)
+}
+
+func (srv *Service) AddUser(user autogen.User) error {
+	return srv.db.AddUser(user)
+}
+
+func (srv *Service) RemoveUser(chatId int64) (bool, error) {
+	return srv.db.RemoveUser(chatId)
+}
+
+func (srv *Service) UpdateUser(chatId int64, updateUser autogen.UpdateUser) (bool, error) {
+	return srv.db.UpdateUser(chatId, updateUser)
+}
